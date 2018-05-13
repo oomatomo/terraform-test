1122a7b8b993a89d07d0f8854297b4cb20abe6c9
oomatomo <ooma0301@gmail.com>
2018-05-13 23:10:18 +0900

    routeテーブル追加

--- main.tf
+++ main.tf
@@ -15,3 +15,20 @@ resource "aws_internet_gateway" "internet_gateway_test" {
     Name = "internet_gateway_test"
   }
 }
+
+data "aws_route_table" "route_table_test" {
+  vpc_id = "${aws_vpc.vpc_test.id}"
+}
+
+#resource "aws_route_table" "route_table_test" {
+#  vpc_id = "${aws_vpc.vpc_test.id}"
+#  tags {
+#      Name = "route_table_test"
+#  }
+#}
+
+resource "aws_route" "route_test" {
+  route_table_id = "${data.aws_route_table.route_table_test.id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = "${aws_internet_gateway.internet_gateway_test.id}"
+}
