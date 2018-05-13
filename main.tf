8ccdd39a18dd765f36c531d153b6eba902bdd0a9
oomatomo <ooma0301@gmail.com>
2018-05-13 23:41:00 +0900

    subnet追加

--- main.tf
+++ main.tf
@@ -32,3 +32,23 @@ resource "aws_route" "route_test" {
   destination_cidr_block = "0.0.0.0/0"
   gateway_id = "${aws_internet_gateway.internet_gateway_test.id}"
 }
+
+resource "aws_subnet" "subnet_test_a" {
+  vpc_id = "${aws_vpc.vpc_test.id}"
+  availability_zone = "ap-northeast-1a"
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = false
+  tags {
+    Name = "subnet_test_a"
+  }
+}
+
+resource "aws_subnet" "subnet_test_c" {
+  vpc_id = "${aws_vpc.vpc_test.id}"
+  availability_zone = "ap-northeast-1c"
+  cidr_block = "10.0.2.0/24"
+  map_public_ip_on_launch = false
+  tags {
+    Name = "subnet_test_c"
+  }
+}
